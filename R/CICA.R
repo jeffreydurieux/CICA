87f9153eb179fb26696fb76f74adf54a9159fc23
jeffreydurieux <durieux.jeffrey@gmail.com>
2021-06-22 15:21:56 +0200

    add CICA main function file. Not finished yet

---
+++ R/CICA.R
@@ -0,0 +1,16 @@
+CICA <- function(DataList, nStarts, nComp, nClus, scale = TRUE,
+                 center = TRUE, rational = NULL, maxiter = 100){
+
+  #### input arguments check ####
+
+  #### Preprocessing ####
+
+  #### Rational starts ####
+
+  #### Random starts ####
+
+  #### output ####
+  #test
+
+
+}
