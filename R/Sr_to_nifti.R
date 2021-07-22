5284724f597f1689c6d3fc81debd194c9226a8d0
jeffreydurieux <durieux.jeffrey@gmail.com>
2021-07-22 15:19:04 +0200

    Added Sr_to_nifti function and plot method.

---
+++ R/Sr_to_nifti.R
@@ -0,0 +1,42 @@
+#' Convert Cluster specific independent components to NIFTI format
+#'
+#' @param x an object of \code{class} CICA
+#'
+#' @return a list with niftiImage files
+#' @export
+#'
+Sr_to_nifti <- function(x){
+
+  dims <- dim(x$Sr[[1]])
+
+  if(dims[1] == 14812){
+    img <- readNifti(file = system.file(package = 'CICA', 'extdata', '8mm.nii.gz', mustWork = TRUE) )
+
+  }else if(dims[1] == 116380){
+    img <- readNifti(file = system.file(package = 'CICA', 'extdata', '4mm.nii.gz', mustWork = TRUE) )
+
+  }else if(dims[1] == 902629){
+    img <- readNifti(file = system.file(package = 'CICA', 'extdata', '2mm.nii.gz', mustWork = TRUE) )
+  }else{
+    warning('Voxel dimension not equal to a the size of a MNI brain template')
+    d <- ceiling(dims[1] ^ (1/3))
+    img <- array(data = 0, dim = c(d,d,d) )
+  }
+
+  dimnif <- dim(img)
+  dimnif[4] <- dim(x$Sr[[1]])[2]
+
+  NIFS <- vector(mode = 'list', length = length(x$Sr))
+  for(i in 1:length(x$Sr)){
+    arr <- array(x$Sr[[i]], dim = dimnif)
+    img <- asNifti(arr, reference = img)
+    NIFS[[i]] <- img
+  }
+
+  names(NIFS) <- paste('Sr_', 1:length(x$Sr), sep='')
+
+  return(NIFS)
+}
+
+
+
--- R/plot.CICA.R
+++ R/plot.CICA.R
@@ -1,11 +1,27 @@
 #' Plot method for
-#' @param what ...
-#' @param res ...
+#' @param x Object of \code{class} CICA
+#' @param brain auto
 #' @export
 #'
 #'
-plot.CICA <- function(what, res){
-  img <- readMNI(what, res)
-  plot(img)
+plot.CICA <- function(x, brain = 'auto'){
+
+  dims <- dim(x$Sr[[1]])
+
+  if(dims[1] == 14812){
+    img <- readNifti(file = system.file(package = 'CICA', 'extdata',
+                                        '8mm.nii.gz', mustWork = TRUE) )
+    papaya( c(list(img), Sr_to_nifti(x) ) )
+  }else if(dims[1] == 116380){
+    img <- readNifti(file = system.file(package = 'CICA', 'extdata',
+                                        '4mm.nii.gz', mustWork = TRUE) )
+    papaya( c(list(img), Sr_to_nifti(x) ) )
+  }else if(dims[1] == 902629){
+    img <- readNifti(file = system.file(package = 'CICA', 'extdata',
+                                        '2mm.nii.gz', mustWork = TRUE) )
+    papaya( c(list(img), Sr_to_nifti(x) ) )
+  }else{
+    stop('Data does not correspond to MNI coordinates, no plot returned')
+  }
 }
 
