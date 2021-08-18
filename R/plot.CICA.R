2ebbee640758a2b981eba9e0bd5ce7f0874f5240
jeffreydurieux <durieux.jeffrey@gmail.com>
2021-08-18 16:33:30 +0200

    Added a plot method option to CICAoutput that is not based on fMRI data. An interactive histogram of the components is shown

--- R/plot.CICA.R
+++ R/plot.CICA.R
@@ -3,11 +3,12 @@
 #' independent components in an interactive viewer using the papayar package
 #' @param x Object of \code{class} CICA
 #' @param brain auto
+#' @param cluster Components of cluster to plot. Only used when non fMRI related data is used
 #' @param ... other arguments
 #' @export
 #'
 #'
-plot.CICA <- function(x, brain = 'auto', ...){
+plot.CICA <- function(x, brain = 'auto', cluster = 1, ...){
 
   dims <- dim(x$Sr[[1]])
 
@@ -24,7 +25,33 @@ plot.CICA <- function(x, brain = 'auto', ...){
                                         '2mm.nii.gz', mustWork = TRUE) )
     papaya( c(list(img), Sr_to_nifti(x) ) )
   }else{
-    stop('Data does not correspond to MNI coordinates, no plot returned')
+    #warning('Data does not correspond to MNI coordinates, no interactive plot returned')
+    df <- data.frame(x$Sr[[cluster]])
+    y_axis_var_names <- paste('Comp' , 1:ncol(x$Sr[[cluster]]), sep = '_')
+    colnames(df) <- y_axis_var_names
+    create_buttons <- function(df, y_axis_var_names) {
+      lapply(
+        y_axis_var_names,
+        FUN = function(var_name, df) {
+          button <- list(
+            method = 'restyle',
+            args = list('x', list(df[, var_name])),
+            label = var_name
+          )
+        },
+        df
+      )
+    }
+
+    fig1 <- plot_ly(df, x = ~Comp_1, alpha = 0.3, type = 'histogram') %>%
+      layout(title = paste('Cluster', cluster),
+             updatemenus = list(
+               list(
+                 buttons = create_buttons(df = df, y_axis_var_names = y_axis_var_names)
+               )
+             ))
+    fig1
+
   }
 }
 
