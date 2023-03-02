d4824e22a3bdc8f989cb53bf756377b2c4b7eca7
Jeffrey Durieux <jeffreydurieux@users.noreply.github.com>
2023-03-02 13:43:14 +0100

    Update ModelSelection.R
    added functionality for either only one Q (selecting optimal R) or one R (selecting optimal Q). Based on simple scree test

--- R/ModelSelection.R
+++ R/ModelSelection.R
@@ -53,29 +53,72 @@ SequentialScree <- function(x){
     return(Screes)
   }
 
-  Qu <- unique(df$Q)
-  Screes <- SR_rq( df[df$Q==Qu[1], ]$Loss )
-  for(i in 2:length(Qu)){
-    Screes <- rbind(Screes, SR_rq(df[df$Q==Qu[i], ]$Loss ))
+
+  if( (length(unique(df$Q))>=3) & (length(unique(df$R))>=3) ){
+    # if both R and Q
+    Qu <- unique(df$Q)
+    Screes <- SR_rq( df[df$Q==Qu[1], ]$Loss )
+    for(i in 2:length(Qu)){
+      Screes <- rbind(Screes, SR_rq(df[df$Q==Qu[i], ]$Loss ))
+    }
+
+    ColMeanScrees <- colMeans(Screes, na.rm = TRUE)
+    Rid <- which.max(ColMeanScrees)
+    Ru <- unique(df$R)
+    Rselect <- Ru[Rid]
+
+    ScreeConditionalonR <- SR_qR(df[df$R==Rselect,]$Loss)
+    Qid <- which.max(ScreeConditionalonR)
+    Qselect <- Qu[Qid]
+
+    out <- list()
+    out$optimalQ <- Qselect
+    out$optimalR <- Rselect
+    out$df <- df
+    out$Screes_step1 <- Screes
+    out$ColMeanScrees <- ColMeanScrees
+    out$ScreeConditionalonR <- ScreeConditionalonR
+    class(out) <- 'ModSel'
+  }else if( (length(unique(df$R))>=3) & (length(unique(df$Q))==1) ){
+
+    dff <- data.frame(R=df$R, Loss=df$Loss)
+    Screes <- numeric()
+
+    for(i in 1:length(dff$Loss)){
+      if(i == 1){
+        Screes[i] <- NA
+      }
+      else if(i == length(dff$Loss) ){
+        Screes[i] <- NA
+      }else{
+        Screes[i] <- (dff$Loss[i-1] - dff$Loss[i]) / (dff$Loss[i] - dff$Loss[i+1])
+      }
+    }
+
+    out <- list()
+    out$optimalR <- df$R[which.max(Screes)]
+    out$df <- dff
+    class(out) <- 'ModSel'
+
+  }else if( (length(unique(df$Q))>=3) & (length(unique(df$R))==1) ){
+    # if only one R
+    dff <- data.frame(Q=df$Q, Loss=df$Loss)
+    Screes <- numeric()
+
+    for(i in 1:length(dff$Loss)){
+      if(i == 1){
+        Screes[i] <- NA
+      }
+      else if(i == length(dff$Loss) ){
+        Screes[i] <- NA
+      }else{
+        Screes[i] <- (dff$Loss[i-1] - dff$Loss[i]) / (dff$Loss[i] - dff$Loss[i+1])
+      }
+    }
+    out <- list()
+    out$optimalQ <- df$Q[which.max(Screes)]
+    out$df <- dff
+    class(out) <- 'ModSel'
   }
-
-  ColMeanScrees <- colMeans(Screes, na.rm = TRUE)
-  Rid <- which.max(ColMeanScrees)
-  Ru <- unique(df$R)
-  Rselect <- Ru[Rid]
-
-  ScreeConditionalonR <- SR_qR(df[df$R==Rselect,]$Loss)
-  Qid <- which.max(ScreeConditionalonR)
-  Qselect <- Qu[Qid]
-
-
-  out <- list()
-  out$optimalQ <- Qselect
-  out$optimalR <- Rselect
-  out$df <- df
-  out$Screes_step1 <- Screes
-  out$ColMeanScrees <- ColMeanScrees
-  out$ScreeConditionalonR <- ScreeConditionalonR
-  class(out) <- 'ModSel'
   return(out)
 }
