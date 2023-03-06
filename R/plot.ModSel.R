33099288973b65ce75bfb16f675cdb01956382bb
Jeffrey Durieux <jeffreydurieux@users.noreply.github.com>
2023-03-06 20:24:57 +0100

    update plot.ModSel
    added arrow for selected model

--- R/plot.ModSel.R
+++ R/plot.ModSel.R
@@ -8,7 +8,19 @@ plot.ModSel <- function(x){
   if(class(x) != 'ModSel'){
     stop('Input object is not of class ModSel')
   }
-  plot_ly(data = x$df, x = ~Q, y=~Loss, color = ~as.factor(R), mode='lines+markers', type = 'scatter')
+
+  xsel <- x$optimalQ
+  rsel <- x$optimalR
+  ysel <- x$df[which(x$df$Q == xsel & x$df$R == rsel),]$Loss
+
+  plot_ly(data = x$df, x = ~Q, y=~Loss, color = ~as.factor(R), mode='lines+markers',
+          type = 'scatter') %>%
+    layout(annotations = list(x = xsel, y = ysel,
+                              showarrow = TRUE, text = 'selected model',
+                              arrowhead = 6),
+           legend = list(title=list(text='Cluster')))
+
+
 
 }
 
